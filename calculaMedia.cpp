b4431bcf7059831a00b90438b072451fa9f98a22
josebairinhas <josebairinhas24@gmail.com>
2023-10-16 12:11:41 +0100

    calculaMedia

---
+++ calculaMedia.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	float nota1,nota2,nota3,nota4,somaNotas,media;
+	cout<<"Insira 4 notas"<<endl;
+	cin>>nota1;
+	cin>>nota2;
+	cin>>nota3;
+	cin>>nota4;
+	somaNotas = nota1+nota2+nota3+nota4;
+	media = somaNotas/4;
+	cout<< "Nota 1: "<< nota1<<endl;
+	cout<< "Nota 2: "<< nota2<<endl;
+	cout<< "Nota 3: "<< nota3<<endl;
+	cout<< "Nota 4: "<< nota4<<endl;
+	cout<< "Media: "<< media<<endl;
+}
